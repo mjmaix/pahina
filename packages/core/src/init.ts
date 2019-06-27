6f77b5b7c993cf92ed66828359b19467d3037557
MJ Abadilla <mjmaix@gmail.com>
2019-06-27 12:35:20 +0800

    [core] * Fixes https://github.com/mjmaix/pahina/issues/1 * manually copy new env file

--- packages/core/src/init.ts
+++ packages/core/src/init.ts
@@ -2,7 +2,8 @@ import awsmobile from './aws-exports';
 import Amplify, { Logger } from 'aws-amplify';
 import Analytics from '@aws-amplify/analytics';
 
-Logger.LOG_LEVEL = 'DEBUG';
-Analytics.disable(); // FIXME: https://github.com/mjmaix/pahina/issues/1
+Logger.LOG_LEVEL = 'INFO';
 
 Amplify.configure(awsmobile);
+
+Analytics.enable();
