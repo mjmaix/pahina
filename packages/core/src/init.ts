1fc9657fedd0dcba132936a53a9ea0a6580ddd76
MJ Abadilla <mjmaix@gmail.com>
2019-06-27 10:52:42 +0800

    [app] * Cannot complete login -  cannot retrieve/query PahinaUser
    [core]
    * Workaround for analytics not accepting unauth policy

--- packages/core/src/init.ts
+++ packages/core/src/init.ts
@@ -1,4 +1,8 @@
 import awsmobile from './aws-exports';
-import Amplify from 'aws-amplify';
+import Amplify, { Logger } from 'aws-amplify';
+import Analytics from '@aws-amplify/analytics';
+
+Logger.LOG_LEVEL = 'DEBUG';
+Analytics.disable(); // FIXME: https://github.com/mjmaix/pahina/issues/1
 
 Amplify.configure(awsmobile);
--- packages/web/aws-exports.js
+++ packages/web/aws-exports.js
@@ -2,21 +2,21 @@
 
 const awsmobile = {
     "aws_project_region": "ap-southeast-1",
-    "aws_cognito_identity_pool_id": "ap-southeast-1:9d9d1bba-5b97-435b-81f8-52473a9e410f",
+    "aws_cognito_identity_pool_id": "ap-southeast-1:84b23718-6c44-4361-b0a1-7ff8c43f2db5",
     "aws_cognito_region": "ap-southeast-1",
-    "aws_user_pools_id": "ap-southeast-1_1FalyrXai",
-    "aws_user_pools_web_client_id": "38cegm54ve6f2m3bd09shvge9g",
+    "aws_user_pools_id": "ap-southeast-1_J5ly3RHtS",
+    "aws_user_pools_web_client_id": "60f226pbqif0ant8t611kg0dsd",
     "oauth": {},
-    "aws_appsync_graphqlEndpoint": "https://a3f7qeeonbfbzlai5lnys4htli.appsync-api.ap-southeast-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://5ypvqw4awjd57fga7wnaz6u7ya.appsync-api.ap-southeast-1.amazonaws.com/graphql",
     "aws_appsync_region": "ap-southeast-1",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
-    "aws_user_files_s3_bucket": "pahina2d2d892b4ceb43b7996e3ea96dfd61cb-redev",
+    "aws_user_files_s3_bucket": "pahina2d2d892b4ceb43b7996e3ea96dfd61cb-bugbug",
     "aws_user_files_s3_bucket_region": "ap-southeast-1",
-    "aws_mobile_analytics_app_id": "8f81b825191f46f898a8efc3eef2fbba",
+    "aws_mobile_analytics_app_id": "09912724f9bb46d38f1be0774549edbe",
     "aws_mobile_analytics_app_region": "us-west-2",
-    "aws_content_delivery_bucket": "pahina-20190624230740-hostingbucket-redev",
+    "aws_content_delivery_bucket": "pahina-20190624230740-hostingbucket-bugbug",
     "aws_content_delivery_bucket_region": "ap-southeast-1",
-    "aws_content_delivery_url": "http://pahina-20190624230740-hostingbucket-redev.s3-website-ap-southeast-1.amazonaws.com"
+    "aws_content_delivery_url": "http://pahina-20190624230740-hostingbucket-bugbug.s3-website-ap-southeast-1.amazonaws.com"
 };
 
 
