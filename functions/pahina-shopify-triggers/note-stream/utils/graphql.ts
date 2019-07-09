90dc4f5a8f234e948c0554dc34b24352f1b28f72
MJ Abadilla <mjmaix@gmail.com>
2019-07-09 13:33:23 +0800

    [functions] pahina-shopify-triggers/note-stream
    * rename variables
    * change stream arn

--- functions/pahina-shopify-triggers/note-stream/utils/graphql.ts
+++ functions/pahina-shopify-triggers/note-stream/utils/graphql.ts
@@ -1,10 +1,10 @@
 class GraphQL {
   endpoint: string;
   constructor() {
-    const hostname = process.env.SHOPIFY_HOSTNAME;
-    const apikey = process.env.SHOPIFY_API_KEY;
-    const password = process.env.SHOPIFY_API_PWD;
-    this.endpoint = `https://${apikey}:${password}@${hostname}/admin/api/2019-07/`;
+    const hostname = process.env.SHOPIFY_API_HOSTNAME;
+    // const apikey = process.env.SHOPIFY_API_KEY;
+    // const password = process.env.SHOPIFY_API_PWD;
+    this.endpoint = `https://${hostname}/admin/api/graphql.json`;
   }
 }
 
